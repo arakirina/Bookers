11141c6a89122d6bac240f8c55e96ddcf710ec39
EC2 Default User <ec2-user@ip-172-31-17-85.ec2.internal>
2021-12-08 13:09:32 +0000

    [update]3-9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,35 @@
 class BooksController < ApplicationController
   def index
+    @book = Book.new
+    @books = Book.all
   end
 
   def edit
   end
 
   def show
+    @book = Book.find(params[:id])
   end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+    redirect_to book_path(@book.id)
+    # URLを確認して（ルートに）アクションに移動するcreateからshowに移動するrais routesで確認
+    else
+    @books = Book.all
+    render :index
+    # アクションは移動せずにcreateアクション内で無理やりindexのページを表示させている
+    # indexに入っているものをrenderの前に入れてあげるあえて使うのは、エラーメッセージを保持するため・入力ができていた方の情報も保持される
+    end
+  end
+
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
+
+
 end
